e66078787d5c98a117cb4bdf047e9efecb3021b9
nachosala <nachosala89@gmail.com>
2021-09-22 23:06:36 -0300

    Center content and fix JS issues

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-// eslint-disable-next-line max-classes-per-file
 import './style.css';
 import '@fortawesome/fontawesome-free/js/fontawesome.js';
 import '@fortawesome/fontawesome-free/js/solid.js';
@@ -13,45 +12,19 @@ class Task {
   }
 }
 
-class Tasks {
-  constructor() {
-    this.list = [];
-    this.lastIndex = 0;
-  }
+const sortList = (tasksList) => tasksList.sort((a, b) => a.index - b.index);
 
-  addTask(description) {
-    this.lastIndex += 1;
-    const task = new Task(this.lastIndex, description);
-    this.list = [...this.list, task];
-  }
-
-  copyList(list) {
-    this.list = [...list];
-  }
-
-  removeTask(task) {
-    const oldList = this.list;
-    const index = this.list.indexOf(task);
-    this.list = [...oldList.slice(0, index), ...oldList.slice(index + 1)];
-  }
-
-  sortList() {
-    this.list.sort((a, b) => a.index - b.index);
-  }
-}
-
-const taskList = new Tasks();
-taskList.copyList([new Task(1, 'wash the dishes'), new Task(2, 'complete To Do list project')]);
+let tasksList = [new Task(1, 'wash the dishes'), new Task(2, 'complete To Do list project')];
 
 const listContainer = document.querySelector('ul');
 
-function showList() {
-  taskList.sortList();
-  taskList.list.forEach((item) => {
+const showList = () => {
+  tasksList = sortList(tasksList);
+  tasksList.forEach((item) => {
     const li = document.createElement('li');
     li.innerHTML = `<input type="checkbox" id="check-${item.index}"><textarea>${item.description}</textarea><i class="fas fa-ellipsis-v"></i>`;
     listContainer.appendChild(li);
   });
-}
+};
 
 showList();
