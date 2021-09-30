88820ebe2a209070ed693339b4072ab2db69d6a8
nachosala <nachosala89@gmail.com>
2021-09-30 16:00:17 -0300

    Test completed status

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,10 +1,14 @@
+import { JSDOM } from 'jsdom';
 import { listAddTask, listRemoveTask } from '../src/listManager.js';
 import Task from '../src/taskClass.js';
-import LocalStorageMock, { markTask } from './localStorage.js';
+import LocalStorageMock from './localStorage.js';
 import { updateStatus } from '../src/updateStatus.js';
 
 global.localStorage = new LocalStorageMock();
-global.markTask = markTask;
+
+const dom = new JSDOM();
+global.document = dom.window.document;
+global.window = dom.window;
 
 describe('Add items', () => {
   test('Add item to empty list', () => {
@@ -20,9 +24,19 @@ describe('Remove items', () => {
     const tasksList = [expectedTask];
     expect(listRemoveTask(tasksList, 0)).toStrictEqual([expectedTask]);
   });
-  test('update item from list', () => {
-    const expectedTask = new Task(1, 'Wash dishes');
-    const tasksList = [expectedTask];
-    expect(updateStatus(tasksList, 0)).toStrictEqual([expectedTask]);
-  });
+});
+
+describe('Update items from list', () => {
+  test('Mark Item as completed', () => {
+
+    const tasksList = [new Task(0, 'Wash dishes'), new Task(1, 'Learn Jest')];
+
+    document.body.innerHTML =
+    '<ul>' +
+      `<li class="task-item item-0"><input type="checkbox"><textarea>${tasksList[0].description}</textarea></li>` +
+      `<li class="task-item item-1"><input type="checkbox"><textarea>${tasksList[1].description}</textarea></li>` +
+    '</ul>';
+    updateStatus(tasksList, 0);
+    expect(tasksList[0].completed).toBe(true);
+  });  
 });
