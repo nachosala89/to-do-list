9b7cafe8fd62f59338b58437952587557c339765
nachosala <nachosala89@gmail.com>
2021-09-30 17:16:46 -0300

    Test edit and remove completed functions

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,5 +1,6 @@
 import { JSDOM } from 'jsdom';
-import { listAddTask, listRemoveTask } from '../src/listManager.js';
+import { listAddTask, listRemoveTask, updateDescription } from '../src/listManager.js';
+import { removeMarked } from '../src/showItems.js';
 import Task from '../src/taskClass.js';
 import LocalStorageMock from './localStorage.js';
 import { updateStatus } from '../src/updateStatus.js';
@@ -27,10 +28,9 @@ describe('Remove items', () => {
 });
 
 describe('Update items from list', () => {
+  const tasksList = [new Task(0, 'Wash dishes'), new Task(1, 'Learn Jest')];
+  
   test('Mark Item as completed', () => {
-
-    const tasksList = [new Task(0, 'Wash dishes'), new Task(1, 'Learn Jest')];
-
     document.body.innerHTML =
     '<ul>' +
       `<li class="task-item item-0"><input type="checkbox"><textarea>${tasksList[0].description}</textarea></li>` +
@@ -38,5 +38,17 @@ describe('Update items from list', () => {
     '</ul>';
     updateStatus(tasksList, 0);
     expect(tasksList[0].completed).toBe(true);
-  });  
+  });
+
+  test('Edit task description', () => {
+    const textInMock = { value: 'Test code' };
+    updateDescription(tasksList, 0, textInMock);
+    expect(tasksList[0].description).toBe('Test code');
+  });
+
+  test('Remove completed tasks', () => {
+    tasksList[1].completed = true;
+    removeMarked(tasksList);
+    expect(tasksList[1].deleted).toBe(true);
+  });
 });
